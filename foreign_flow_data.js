b464e7cd43ca61ef7a44529dd5e1d9c787ea9f51
김수철 <bracekim@naver.com>
2026-09-23 10:50:02 +0900

    Update reports

--- foreign_flow_data.js
+++ foreign_flow_data.js
@@ -2,9 +2,9 @@
 // fetch_foreign_flow.py 가 매 실행마다 이 파일을 덮어씁니다.
 window.__EWY_FOREIGN_FLOW__ = {
   "date": "20260923",
-  "fetched_at": "2026-09-23 10:40:05",
+  "fetched_at": "2026-09-23 10:45:04",
   "kospi_market": {
-    "순매수": -42218993902,
+    "순매수": -60084151107,
     "기준일": "20260923"
   },
   "tickers": {
